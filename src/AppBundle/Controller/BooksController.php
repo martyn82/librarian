e3d4c57271f2f5fbbe04e8189c036b91916cbd86
Martijn Endenburg <martijn.endenburg@gmail.com>
2015-07-19 13:09:32 +0200

    Implement read action

--- src/AppBundle/Controller/BooksController.php
+++ src/AppBundle/Controller/BooksController.php
@@ -3,13 +3,17 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Domain\Service\BookService;
+use AppBundle\Domain\Service\ObjectNotFoundException;
+use AppBundle\EventStore\Uuid;
 use AppBundle\MessageBus\CommandBus;
-use FOS\RestBundle\Controller\Annotations as REST;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Request\ParamFetcher;
 use JMS\DiExtraBundle\Annotation as DI;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
- * @REST\Route("/api")
+ * @Rest\Route("/api")
  */
 class BooksController extends FOSRestController
 {
@@ -39,13 +43,32 @@ class BooksController extends FOSRestController
     }
 
     /**
-     * @REST\Get("/books")
-     * @REST\View()
+     * @Rest\Get("/books")
+     * @Rest\View()
      *
      * @return array
      */
     public function indexAction()
     {
         return $this->bookService->getAll();
+    }
+
+    /**
+     * @Rest\Get("/book/{id}",
+     *  requirements={"id"="[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}"},
+     *  defaults={"id"=null}
+     * )
+     * @Rest\View()
+     *
+     * @return Book
+     * @throws HttpException
+     */
+    public function readAction($id)
+    {
+        try {
+            return $this->bookService->getBook(Uuid::deserialize(['value' => $id]));
+        } catch (ObjectNotFoundException $e) {
+            throw $this->createNotFoundException($e->getMessage(), $e);
+        }
     }
 }
